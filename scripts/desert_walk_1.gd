25fc513de909f93fcace4ef920f52d012cdef4dd
fr3ising <freising.denuevo@gmail.com>
2025-08-07 17:20:50 +0200

    Add inventory and desert sky

--- scripts/desert_walk_1.gd
+++ scripts/desert_walk_1.gd
@@ -8,19 +8,19 @@ var travis
 var camera
 
 func _ready() -> void:
+	# var desert_sky = DesertSkyScene.instantiate()
+	# add_child(desert_sky)
 	var desert = DesertScene.instantiate()
 	add_child(desert)
-	var desert_sky = DesertSkyScene.instantiate()
-	add_child(desert_sky)
 	var travis_spawn_point = desert.get_node("TravisSpawnPoint")
 	travis = TravisScene.instantiate()
 	travis.global_position = travis_spawn_point.global_position
 	add_child(travis)
 	var travis_body = travis.get_node("TravisBody")
 	camera = Camera2D.new()
-	camera.position_smoothing_enabled = false
 	camera.make_current()
 	travis_body.add_child(camera)
+	camera.position_smoothing_enabled = false
 
 func _process(_delta: float) -> void:
 	var travis_body = travis.get_node("TravisBody")
